7e26d8b807ad76b5fb266404d4cf7f99e5379a57
Andrejk98 <andrejk98@web.de>
2021-11-30 09:37:50 +0100

    fehlerhaft

--- Aufgabe5/script.js
+++ Aufgabe5/script.js
@@ -1,14 +1,19 @@
 "use strict";
-var localStorageübung;
-(function (localStorageübung) {
-    const inputfield = (document.getElementById("input-Element"));
+var Aufgabe5;
+(function (Aufgabe5) {
+    const interpretInput = (document.querySelector("#interpret"));
+    const priceInput = (document.querySelector("#price"));
+    const enterbutton = (document.querySelector("#enter"));
+    const tbody = (document.querySelector("tbody"));
     const saveButton = (document.getElementById("save-Button"));
     const loadButton = (document.getElementById("load-button"));
     const display = (document.getElementById("display"));
     function saveButtonHandler() {
         console.log("save");
-        inputfield.value;
-        localStorage.setItem("storage-input", inputfield.value);
+        interpretInput.value;
+        localStorage.setItem("storage-input", interpretInput.value);
+        priceInput.value;
+        localStorage.setItem("storage-input", priceInput.value);
     }
     function loadButtonHandler() {
         console.log("load");
@@ -16,7 +21,27 @@ var localStorageübung;
         console.log(localValue);
         display.textContent = localValue;
     }
+    function enterEvent(_evt) {
+        _evt.preventDefault();
+        console.log(interpretInput.value);
+        console.log(priceInput.value);
+    }
+    function addRow(_e) {
+        _e.preventDefault();
+        tbody.innerHTML += `<tr><td>${interpretInput.value}</td><td>${priceInput.value}</td><td><button>Delete</button></td></tr>`;
+        let allButtons = document.querySelectorAll("button");
+        for (const ele of allButtons) {
+            ele.addEventListener("click", function () {
+                this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement);
+            });
+        }
+    }
+    /*function deleteItem(ele: HTMLButtonElement): void{
+        ele.parentElement.parentElement.parentElement.removeChild(ele.parentElement.parentElement);
+    }*/
     saveButton.addEventListener("click", saveButtonHandler);
     loadButton.addEventListener("click", loadButtonHandler);
-})(localStorageübung || (localStorageübung = {}));
+    enterbutton.addEventListener("click", enterEvent);
+    enterbutton.addEventListener("click", addRow);
+})(Aufgabe5 || (Aufgabe5 = {}));
 //# sourceMappingURL=script.js.map
--- Aufgabe5/script.ts
+++ Aufgabe5/script.ts
@@ -1,6 +1,19 @@
-namespace localStorageübung{
-    const inputfield: HTMLInputElement = <HTMLInputElement>(
-        document.getElementById("input-Element")
+namespace Aufgabe5{
+    
+    const interpretInput: HTMLInputElement = <HTMLInputElement>(
+        document.querySelector("#interpret")
+    );
+    
+    const priceInput: HTMLInputElement = <HTMLInputElement>(
+        document.querySelector("#price")
+    );
+        
+    const enterbutton: HTMLElement = <HTMLElement>(
+       document.querySelector("#enter") 
+    );
+        
+    const tbody: HTMLTableSectionElement = <HTMLTableSectionElement>(
+        document.querySelector("tbody")
     );
 
     const saveButton: HTMLButtonElement = <HTMLButtonElement>(
@@ -17,8 +30,10 @@ namespace localStorageübung{
 
     function saveButtonHandler(): void{
         console.log("save")
-        inputfield.value;
-        localStorage.setItem("storage-input", inputfield.value);
+        interpretInput.value;
+        localStorage.setItem("storage-input", interpretInput.value);
+        priceInput.value;
+        localStorage.setItem("storage-input", priceInput.value);
     }
 
     function loadButtonHandler(): void{
@@ -27,7 +42,33 @@ namespace localStorageübung{
         console.log(localValue);
         display.textContent = localValue;
     }
+
+    function enterEvent(_evt: Event){
+        _evt.preventDefault();
+        console.log(interpretInput.value);
+        console.log(priceInput.value);
+    }
+    
+    function addRow(_e: Event){
+    
+        _e.preventDefault();
+        tbody.innerHTML += `<tr><td>${interpretInput.value}</td><td>${priceInput.value}</td><td><button>Delete</button></td></tr>`;
+    
+        let allButtons: NodeListOf<HTMLButtonElement> = document.querySelectorAll("button");
+    
+        for ( const ele of allButtons ) {
+            ele.addEventListener("click", function() {
+                this.parentElement.parentElement.parentElement.removeChild(this.parentElement.parentElement);
+            });
+        }        
+    }
+    
+    /*function deleteItem(ele: HTMLButtonElement): void{
+        ele.parentElement.parentElement.parentElement.removeChild(ele.parentElement.parentElement);
+    }*/
     
     saveButton.addEventListener("click", saveButtonHandler);
     loadButton.addEventListener("click", loadButtonHandler);
+    enterbutton.addEventListener("click", enterEvent);
+    enterbutton.addEventListener("click", addRow);
 }
